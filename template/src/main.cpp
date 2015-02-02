25558530c61d340b89eda02028cf516a11b2d403
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2015-02-02 10:07:11 -0500

    added Linux/Windows defines

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,7 +1,20 @@
+/*
+ * Auteur:
+ * Date  : 
+ */
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+
+#ifdef __linux__
+    cout << "Veuillez appuyer sur une touche pour continuer...";
+    cin.get();
+#else
+    system("pause");
+#endif
+
     return 0;
 }
