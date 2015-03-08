2b432a76439946d4b6905bc64ec71e6606d46da3
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2015-03-08 17:48:38 -0400

    Placeholder

--- semaine-06/project-01/src/main.cpp
+++ semaine-06/project-01/src/main.cpp
@@ -1,20 +1,7 @@
-/*
- * Auteur:
- * Date  :
- */
-
 #include <iostream>
 
 using namespace std;
 
 int main() {
-
-#ifdef __linux__
-    cout << "Veuillez appuyer sur entré pour continuer...";
-    cin.get();
-#else
-    system("pause");
-#endif
-
     return 0;
 }
